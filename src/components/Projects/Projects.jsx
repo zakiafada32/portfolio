3ebf4dfd1174464fe903f5bacd3a804f7fda17af
zakiafada32 <afadazaki32@gmail.com>
2020-12-05 14:59:44 +0700

    add icon and change page theme

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -62,7 +62,7 @@ const Projects = () => {
                         <a
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="cta-btn text-color-main"
+                          className="cta-btn source-code-color"
                           href={repo}
                         >
                           Source Code
